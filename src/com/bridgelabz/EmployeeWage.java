4c1b022131b5ca10eaa40b12f9f7ae1b93d73fc1
sriram036 <sriramravichandran2608@gmail.com>
2023-07-19 23:24:14 +0530

    Added: Part time

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,10 +2,15 @@ package com.bridgelabz;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        int isPresent = (int)(Math.random()*10)%2;
+        int isPresent = (int)(Math.random()*3-1+1)+1;
         int employeeWage = 0;
-        if (isPresent == 0) {
+        if (isPresent == 1) {
             System.out.println("Employee is Absent");
+            System.out.println("Employee Wage is " + employeeWage);
+        }
+        else if (isPresent == 2) {
+            System.out.println("Employee is part time");
+            employeeWage = 4 * 20;
             System.out.println("Employee Wage is " + employeeWage);
         }
         else {
