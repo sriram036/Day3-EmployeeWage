a77f57344b391db1c4b044e249e9ee5945c989c3
sriram036 <sriramravichandran2608@gmail.com>
2023-07-20 20:36:05 +0530

    Added: Monthly Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,19 +4,27 @@ public class EmployeeWage {
     public static void main(String[] args) {
         int isPresent = (int)(Math.random()*3-1+1)+1;
         int employeeWage = 0;
-        if (isPresent == 1) {
-            System.out.println("Employee is Absent");
-            System.out.println("Employee Wage is " + employeeWage);
-        }
-        else if (isPresent == 2) {
-            System.out.println("Employee is part time");
-            employeeWage = 4 * 20;
-            System.out.println("Employee Wage is " + employeeWage);
-        }
-        else {
-            System.out.println("Employee is present");
-            employeeWage = 8 * 20;
-            System.out.println("Employee Wage is " + employeeWage);
+        int monthWage = 0;
+        switch (isPresent) {
+            case 1: {
+                System.out.println("Employee is Absent");
+                System.out.println("Employee Wage is " + employeeWage);
+                System.out.println("Employee Monthly Wage is " + monthWage);
+            }break;
+            case 2: {
+                System.out.println("Employee is part time");
+                employeeWage = 4 * 20;
+                monthWage = employeeWage * 20;
+                System.out.println("Employee Wage is " + employeeWage);
+                System.out.println("Employee Monthly Wage is " + monthWage);
+            }break;
+            case 3: {
+                System.out.println("Employee is present");
+                employeeWage = 8 * 20;
+                monthWage = employeeWage * 20;
+                System.out.println("Employee Wage is " + employeeWage);
+                System.out.println("Employee Monthly Wage is " + monthWage);
+            }break;
         }
     }
 }
