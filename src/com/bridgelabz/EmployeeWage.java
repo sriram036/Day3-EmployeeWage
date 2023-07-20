4b1b8c0ce3a1ca4400b7bf2a1cd01348465773c2
sriram036 <sriramravichandran2608@gmail.com>
2023-07-20 20:59:38 +0530

    Added: Wages Till Condition

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,30 +1,48 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+
+    public static final int PARTTIME = 4;
+    public static final int FULLTIME = 8;
     public static void main(String[] args) {
-        int isPresent = (int)(Math.random()*3-1+1)+1;
         int employeeWage = 0;
         int monthWage = 0;
-        switch (isPresent) {
-            case 1: {
-                System.out.println("Employee is Absent");
-                System.out.println("Employee Wage is " + employeeWage);
-                System.out.println("Employee Monthly Wage is " + monthWage);
-            }break;
-            case 2: {
-                System.out.println("Employee is part time");
-                employeeWage = 4 * 20;
-                monthWage = employeeWage * 20;
-                System.out.println("Employee Wage is " + employeeWage);
-                System.out.println("Employee Monthly Wage is " + monthWage);
-            }break;
-            case 3: {
-                System.out.println("Employee is present");
-                employeeWage = 8 * 20;
-                monthWage = employeeWage * 20;
-                System.out.println("Employee Wage is " + employeeWage);
-                System.out.println("Employee Monthly Wage is " + monthWage);
-            }break;
+        int workingHours = 0;
+        int noOfDays = 0;
+        int totalWage = 0;
+
+        while (workingHours <= 100 && noOfDays <= 20) {
+            int isPresent = (int)(Math.random()*3-1+1)+1;
+            switch (isPresent) {
+                case 1: {
+                    System.out.println("Employee is Absent");
+                    System.out.println("Employee Wage is " + employeeWage);
+                    System.out.println("Employee Monthly Wage is " + monthWage);
+                }break;
+                case 2: {
+                    System.out.println("Employee is part time");
+                    employeeWage = 4 * 20;
+                    monthWage = employeeWage * 20;
+                    System.out.println("Employee Wage is " + employeeWage);
+                    System.out.println("Employee Monthly Wage is " + monthWage);
+                    workingHours += PARTTIME;
+                    noOfDays++;
+                    totalWage += employeeWage;
+                }break;
+                case 3: {
+                    System.out.println("Employee is present");
+                    employeeWage = 8 * 20;
+                    monthWage = employeeWage * 20;
+                    System.out.println("Employee Wage is " + employeeWage);
+                    System.out.println("Employee Monthly Wage is " + monthWage);
+                    workingHours += FULLTIME;
+                    noOfDays++;
+                    totalWage += employeeWage;
+                }break;
+            }
         }
+        System.out.println("The total Wage is " + totalWage);
+        System.out.println("The total number of working hours is " + workingHours);
+        System.out.println("The number of working days is "+ noOfDays);
     }
 }
